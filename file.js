46feece17ab7b7d2c5d79cb786b577b8a426faef
Natasha <n.e.dolgih@yandex.ru>
2017-11-14 23:43:07 +0500

    fix

--- file.js
+++ file.js
@@ -58,6 +58,632 @@ let useful = {
         "A": "U",
         "G": "C",
         "C": "G"
+    },
+    "blosum62": {
+        "-": {
+            "-": 1,
+            "A": -4,
+            "C": -4,
+            "B": -4,
+            "E": -4,
+            "D": -4,
+            "G": -4,
+            "F": -4,
+            "I": -4,
+            "H": -4,
+            "K": -4,
+            "M": -4,
+            "L": -4,
+            "N": -4,
+            "Q": -4,
+            "P": -4,
+            "S": -4,
+            "R": -4,
+            "T": -4,
+            "W": -4,
+            "V": -4,
+            "Y": -4,
+            "X": -4,
+            "Z": -4
+        },
+        "A": {
+            "-": -4,
+            "A": 4,
+            "C": 0,
+            "B": -2,
+            "E": -1,
+            "D": -2,
+            "G": 0,
+            "F": -2,
+            "I": -1,
+            "H": -2,
+            "K": -1,
+            "M": -1,
+            "L": -1,
+            "N": -2,
+            "Q": -1,
+            "P": -1,
+            "S": 1,
+            "R": -1,
+            "T": 0,
+            "W": -3,
+            "V": 0,
+            "Y": -2,
+            "X": 0,
+            "Z": -1
+        },
+        "C": {
+            "-": -4,
+            "A": 0,
+            "C": 9,
+            "B": -3,
+            "E": -4,
+            "D": -3,
+            "G": -3,
+            "F": -2,
+            "I": -1,
+            "H": -3,
+            "K": -3,
+            "M": -1,
+            "L": -1,
+            "N": -3,
+            "Q": -3,
+            "P": -3,
+            "S": -1,
+            "R": -3,
+            "T": -1,
+            "W": -2,
+            "V": -1,
+            "Y": -2,
+            "X": -2,
+            "Z": -3
+        },
+        "B": {
+            "-": -4,
+            "A": -2,
+            "C": -3,
+            "B": 4,
+            "E": 1,
+            "D": 4,
+            "G": -1,
+            "F": -3,
+            "I": -3,
+            "H": 0,
+            "K": 0,
+            "M": -3,
+            "L": -4,
+            "N": 3,
+            "Q": 0,
+            "P": -2,
+            "S": 0,
+            "R": -1,
+            "T": -1,
+            "W": -4,
+            "V": -3,
+            "Y": -3,
+            "X": -1,
+            "Z": 1
+        },
+        "E": {
+            "-": -4,
+            "A": -1,
+            "C": -4,
+            "B": 1,
+            "E": 5,
+            "D": 2,
+            "G": -2,
+            "F": -3,
+            "I": -3,
+            "H": 0,
+            "K": 1,
+            "M": -2,
+            "L": -3,
+            "N": 0,
+            "Q": 2,
+            "P": -1,
+            "S": 0,
+            "R": 0,
+            "T": -1,
+            "W": -3,
+            "V": -2,
+            "Y": -2,
+            "X": -1,
+            "Z": 4
+        },
+        "D": {
+            "-": -4,
+            "A": -2,
+            "C": -3,
+            "B": 4,
+            "E": 2,
+            "D": 6,
+            "G": -1,
+            "F": -3,
+            "I": -3,
+            "H": -1,
+            "K": -1,
+            "M": -3,
+            "L": -4,
+            "N": 1,
+            "Q": 0,
+            "P": -1,
+            "S": 0,
+            "R": -2,
+            "T": -1,
+            "W": -4,
+            "V": -3,
+            "Y": -3,
+            "X": -1,
+            "Z": 1
+        },
+        "G": {
+            "-": -4,
+            "A": 0,
+            "C": -3,
+            "B": -1,
+            "E": -2,
+            "D": -1,
+            "G": 6,
+            "F": -3,
+            "I": -4,
+            "H": -2,
+            "K": -2,
+            "M": -3,
+            "L": -4,
+            "N": 0,
+            "Q": -2,
+            "P": -2,
+            "S": 0,
+            "R": -2,
+            "T": -2,
+            "W": -2,
+            "V": -3,
+            "Y": -3,
+            "X": -1,
+            "Z": -2
+        },
+        "F": {
+            "-": -4,
+            "A": -2,
+            "C": -2,
+            "B": -3,
+            "E": -3,
+            "D": -3,
+            "G": -3,
+            "F": 6,
+            "I": 0,
+            "H": -1,
+            "K": -3,
+            "M": 0,
+            "L": 0,
+            "N": -3,
+            "Q": -3,
+            "P": -4,
+            "S": -2,
+            "R": -3,
+            "T": -2,
+            "W": 1,
+            "V": -1,
+            "Y": 3,
+            "X": -1,
+            "Z": -3
+        },
+        "I": {
+            "-": -4,
+            "A": -1,
+            "C": -1,
+            "B": -3,
+            "E": -3,
+            "D": -3,
+            "G": -4,
+            "F": 0,
+            "I": 4,
+            "H": -3,
+            "K": -3,
+            "M": 1,
+            "L": 2,
+            "N": -3,
+            "Q": -3,
+            "P": -3,
+            "S": -2,
+            "R": -3,
+            "T": -1,
+            "W": -3,
+            "V": 3,
+            "Y": -1,
+            "X": -1,
+            "Z": -3
+        },
+        "H": {
+            "-": -4,
+            "A": -2,
+            "C": -3,
+            "B": 0,
+            "E": 0,
+            "D": -1,
+            "G": -2,
+            "F": -1,
+            "I": -3,
+            "H": 8,
+            "K": -1,
+            "M": -2,
+            "L": -3,
+            "N": 1,
+            "Q": 0,
+            "P": -2,
+            "S": -1,
+            "R": 0,
+            "T": -2,
+            "W": -2,
+            "V": -3,
+            "Y": 2,
+            "X": -1,
+            "Z": 0
+        },
+        "K": {
+            "-": -4,
+            "A": -1,
+            "C": -3,
+            "B": 0,
+            "E": 1,
+            "D": -1,
+            "G": -2,
+            "F": -3,
+            "I": -3,
+            "H": -1,
+            "K": 5,
+            "M": -1,
+            "L": -2,
+            "N": 0,
+            "Q": 1,
+            "P": -1,
+            "S": 0,
+            "R": 2,
+            "T": -1,
+            "W": -3,
+            "V": -2,
+            "Y": -2,
+            "X": -1,
+            "Z": 1
+        },
+        "M": {
+            "-": -4,
+            "A": -1,
+            "C": -1,
+            "B": -3,
+            "E": -2,
+            "D": -3,
+            "G": -3,
+            "F": 0,
+            "I": 1,
+            "H": -2,
+            "K": -1,
+            "M": 5,
+            "L": 2,
+            "N": -2,
+            "Q": 0,
+            "P": -2,
+            "S": -1,
+            "R": -1,
+            "T": -1,
+            "W": -1,
+            "V": 1,
+            "Y": -1,
+            "X": -1,
+            "Z": -1
+        },
+        "L": {
+            "-": -4,
+            "A": -1,
+            "C": -1,
+            "B": -4,
+            "E": -3,
+            "D": -4,
+            "G": -4,
+            "F": 0,
+            "I": 2,
+            "H": -3,
+            "K": -2,
+            "M": 2,
+            "L": 4,
+            "N": -3,
+            "Q": -2,
+            "P": -3,
+            "S": -2,
+            "R": -2,
+            "T": -1,
+            "W": -2,
+            "V": 1,
+            "Y": -1,
+            "X": -1,
+            "Z": -3
+        },
+        "N": {
+            "-": -4,
+            "A": -2,
+            "C": -3,
+            "B": 3,
+            "E": 0,
+            "D": 1,
+            "G": 0,
+            "F": -3,
+            "I": -3,
+            "H": 1,
+            "K": 0,
+            "M": -2,
+            "L": -3,
+            "N": 6,
+            "Q": 0,
+            "P": -2,
+            "S": 1,
+            "R": 0,
+            "T": 0,
+            "W": -4,
+            "V": -3,
+            "Y": -2,
+            "X": -1,
+            "Z": 0
+        },
+        "Q": {
+            "-": -4,
+            "A": -1,
+            "C": -3,
+            "B": 0,
+            "E": 2,
+            "D": 0,
+            "G": -2,
+            "F": -3,
+            "I": -3,
+            "H": 0,
+            "K": 1,
+            "M": 0,
+            "L": -2,
+            "N": 0,
+            "Q": 5,
+            "P": -1,
+            "S": 0,
+            "R": 1,
+            "T": -1,
+            "W": -2,
+            "V": -2,
+            "Y": -1,
+            "X": -1,
+            "Z": 3
+        },
+        "P": {
+            "-": -4,
+            "A": -1,
+            "C": -3,
+            "B": -2,
+            "E": -1,
+            "D": -1,
+            "G": -2,
+            "F": -4,
+            "I": -3,
+            "H": -2,
+            "K": -1,
+            "M": -2,
+            "L": -3,
+            "N": -2,
+            "Q": -1,
+            "P": 7,
+            "S": -1,
+            "R": -2,
+            "T": -1,
+            "W": -4,
+            "V": -2,
+            "Y": -3,
+            "X": -2,
+            "Z": -1
+        },
+        "S": {
+            "-": -4,
+            "A": 1,
+            "C": -1,
+            "B": 0,
+            "E": 0,
+            "D": 0,
+            "G": 0,
+            "F": -2,
+            "I": -2,
+            "H": -1,
+            "K": 0,
+            "M": -1,
+            "L": -2,
+            "N": 1,
+            "Q": 0,
+            "P": -1,
+            "S": 4,
+            "R": -1,
+            "T": 1,
+            "W": -3,
+            "V": -2,
+            "Y": -2,
+            "X": 0,
+            "Z": 0
+        },
+        "R": {
+            "-": -4,
+            "A": -1,
+            "C": -3,
+            "B": -1,
+            "E": 0,
+            "D": -2,
+            "G": -2,
+            "F": -3,
+            "I": -3,
+            "H": 0,
+            "K": 2,
+            "M": -1,
+            "L": -2,
+            "N": 0,
+            "Q": 1,
+            "P": -2,
+            "S": -1,
+            "R": 5,
+            "T": -1,
+            "W": -3,
+            "V": -3,
+            "Y": -2,
+            "X": -1,
+            "Z": 0
+        },
+        "T": {
+            "-": -4,
+            "A": 0,
+            "C": -1,
+            "B": -1,
+            "E": -1,
+            "D": -1,
+            "G": -2,
+            "F": -2,
+            "I": -1,
+            "H": -2,
+            "K": -1,
+            "M": -1,
+            "L": -1,
+            "N": 0,
+            "Q": -1,
+            "P": -1,
+            "S": 1,
+            "R": -1,
+            "T": 5,
+            "W": -2,
+            "V": 0,
+            "Y": -2,
+            "X": 0,
+            "Z": -1
+        },
+        "W": {
+            "-": -4,
+            "A": -3,
+            "C": -2,
+            "B": -4,
+            "E": -3,
+            "D": -4,
+            "G": -2,
+            "F": 1,
+            "I": -3,
+            "H": -2,
+            "K": -3,
+            "M": -1,
+            "L": -2,
+            "N": -4,
+            "Q": -2,
+            "P": -4,
+            "S": -3,
+            "R": -3,
+            "T": -2,
+            "W": 11,
+            "V": -3,
+            "Y": 2,
+            "X": -2,
+            "Z": -3
+        },
+        "V": {
+            "-": -4,
+            "A": 0,
+            "C": -1,
+            "B": -3,
+            "E": -2,
+            "D": -3,
+            "G": -3,
+            "F": -1,
+            "I": 3,
+            "H": -3,
+            "K": -2,
+            "M": 1,
+            "L": 1,
+            "N": -3,
+            "Q": -2,
+            "P": -2,
+            "S": -2,
+            "R": -3,
+            "T": 0,
+            "W": -3,
+            "V": 4,
+            "Y": -1,
+            "X": -1,
+            "Z": -2
+        },
+        "Y": {
+            "-": -4,
+            "A": -2,
+            "C": -2,
+            "B": -3,
+            "E": -2,
+            "D": -3,
+            "G": -3,
+            "F": 3,
+            "I": -1,
+            "H": 2,
+            "K": -2,
+            "M": -1,
+            "L": -1,
+            "N": -2,
+            "Q": -1,
+            "P": -3,
+            "S": -2,
+            "R": -2,
+            "T": -2,
+            "W": 2,
+            "V": -1,
+            "Y": 7,
+            "X": -1,
+            "Z": -2
+        },
+        "X": {
+            "-": -4,
+            "A": 0,
+            "C": -2,
+            "B": -1,
+            "E": -1,
+            "D": -1,
+            "G": -1,
+            "F": -1,
+            "I": -1,
+            "H": -1,
+            "K": -1,
+            "M": -1,
+            "L": -1,
+            "N": -1,
+            "Q": -1,
+            "P": -2,
+            "S": 0,
+            "R": -1,
+            "T": 0,
+            "W": -2,
+            "V": -1,
+            "Y": -1,
+            "X": -1,
+            "Z": -1
+        },
+        "Z": {
+            "-": -4,
+            "A": -1,
+            "C": -3,
+            "B": 1,
+            "E": 4,
+            "D": 1,
+            "G": -2,
+            "F": -3,
+            "I": -3,
+            "H": 0,
+            "K": 1,
+            "M": -1,
+            "L": -3,
+            "N": 0,
+            "Q": 3,
+            "P": -1,
+            "S": 0,
+            "R": 0,
+            "T": -1,
+            "W": -3,
+            "V": -2,
+            "Y": -2,
+            "X": -1,
+            "Z": 4
+        }
     }
 };
 
@@ -172,7 +798,6 @@ function find_prot_dna(str) {
     let results = matchOverlap(amins, pat)
     return results
 }
-
 
 function revp(str) {
     let res = [];
@@ -471,38 +1096,136 @@ function get_ind_edge(v, graph) {
     }
 }
 
-// while (rebra.length > 0) {
-//     let v = result[result.length - 1];
-//
-//     loop:
-//         for (let i = 0; i < rebra.length; i++) {
-//             console.log(v, rebra[i][0], result)
-//             if (rebra[i][0] == v) {
-//                 result.push(rebra[i][1]);
-//                 rebra.splice(i, 1);
-//                 break loop;
-//             }
-//         }
-// }
-// function findEulerPath(v) {
-//     if (rebra[v]) {
-//
-//         // console.log(v)
-//         let u = rebra[v].slice();
-//         delete rebra[v];
-//         u.forEach(findEulerPath);
-//     }
-//     // console.log(v, result.length, result, JSON.stringify(rebra))
-//     result.push(v)
-//
-//     return;
-// }
-// findEulerPath(0)
-//
-// console.log(result.reverse().join(' -> '))
+function levenshtein(s1, s2, costs) {
+    let i, j, l1, l2, flip, ch, chl, ii, ii2, cost, cutHalf;
+    l1 = s1.length;
+    l2 = s2.length;
+
+    costs = costs || {};
+    let cr = costs.replace || 1;
+    let cri = costs.replaceCase || costs.replace || 1;
+    let ci = costs.insert || 1;
+    let cd = costs.remove || 1;
+
+    cutHalf = flip = Math.max(l1, l2);
+
+    let minCost = Math.min(cd, ci, cr);
+    let minD = Math.max(minCost, (l1 - l2) * cd);
+    let minI = Math.max(minCost, (l2 - l1) * ci);
+    let buf = new Array((cutHalf * 2) - 1);
+
+    for (i = 0; i <= l2; ++i) {
+        buf[i] = i * minD;
+    }
+
+    for (i = 0; i < l1; ++i, flip = cutHalf - flip) {
+        ch = s1[i];
+        chl = ch.toLowerCase();
+
+        buf[flip] = (i + 1) * minI;
+
+        ii = flip;
+        ii2 = cutHalf - flip;
+
+        for (j = 0; j < l2; ++j, ++ii, ++ii2) {
+            cost = (ch === s2[j] ? 0 : (chl === s2[j].toLowerCase()) ? cri : cr);
+            buf[ii + 1] = Math.min(buf[ii2 + 1] + cd, buf[ii] + ci, buf[ii2] + cost);
+        }
+    }
+    return buf[l2 + cutHalf - flip];
+}
+
+function ba5g(str) {
+    let [s1, s2] = str.split('\n').map(e => e.trim());
+    console.log(levenshtein(s1, s2))
+}
+
+function  needleman_wunsch(s1, s2, matrix = useful.blosum62) {
+
+}
+
+function ba5e(str) {
+    let [s1, s2] = str.split('\n').map(e => e.trim());
+    allign_prot(s1, s2);
+}
+
+function allign_prot(s,  t, indel = 5) {
+    // Uses BLOSUM62. PASS indel AS POSITIVE INTEGER, NOT NEGATIVE!
+    let S = [];
+    let opt = []; // 1 = right, 2 = down, 3 = diag
+    S[0] = [];
+    opt[0] = [];
+    S[0][0] = 0;
+    for(let i = 1; i <= s.length; ++i) {
+        S[i] = [];
+        opt[i] = [];
+        S[i][0] = S[i-1][0] - indel;
+        opt[i][0] = 1;
+    }
+    for(let j = 1; j <= t.length; ++j) {
+        S[0][j] = S[0][j-1] - indel;
+        opt[0][j] = 2;
+    }
+    for(let i = 1; i <= s.length; ++i) {
+        for(let j = 1; j <= t.length; ++j) {
+            let opt1 = S[i][j-1] - indel; // right
+            let opt2 = S[i-1][j] - indel; // down
+            let opt3 = S[i-1][j-1] + useful.blosum62[s[i-1]][t[j-1]]; // diag
+            S[i][j] = opt1;
+            opt[i][j] = 1;
+            if(opt2 > S[i][j]) {
+                S[i][j] = opt2;
+                opt[i][j] = 2;
+            }
+            if(opt3 > S[i][j]) {
+                S[i][j] = opt3;
+                opt[i][j] = 3;
+            }
+        }
+    }
+    let out = ['', ''];
+    let count = S[s.length][t.length];
+
+    let i = s.length;
+    let j = t.length;
+    while(i > 0 && j > 0) {
+        if(opt[i][j] == 1) { // right
+            out[0] = '-' + out[0];
+            out[1] = t[j-- - 1] + out[1];
+        }
+        else if(opt[i][j] == 2) { // down
+            out[0] = s[i-- - 1] + out[0];
+            out[1] = '-' + out[1];
+        }
+        else if(opt[i][j] == 3) { // diag
+            out[0] = s[i-- - 1] + out[0];
+            out[1] = t[j-- - 1] + out[1];
+        }
+    }
+    if(i > 0) {
+        out[0] = s.substring(0,i) + out[0];
+        let add = "";
+        for(let x = 0; x < i; ++x) {
+            add += '-';
+        }
+        out[1] = add + out[1];
+    }
+    if(j > 0) {
+        out[1] = t.substring(0,j) + out[1];
+        let add = "";
+        for(let x = 0; x < j; ++x) {
+            add += '-';
+        }
+        out[0] = add + out[0];
+    }
+    console.log(count)
+    console.log(out[0])
+    console.log(out[1])
+    return out[0];
+}
 
 
 // fs.readFile('tmp.txt', (err, data) => {
-fs.readFile('rosalind_ba3f.txt', (err, data) => {
-    ba3f(data.toString());
+fs.readFile('rosalind_ba5e.txt', (err, data) => {
+    ba5e(data.toString());
 });
